a46a4a91a4b537f32c86e49e38360d3ecbd76bc4
Berzeg07 <berezg86@gmail.com>
2017-08-10 12:53:37 +0300

    10.08

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
+</head>
+<body>
+
+<h1>Заголовок</h1>
+
+
+
+
+
+
+
+	<!-- JS_BLOCK -->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/owl.carousel/owl.carousel.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/fonts.js"></script>
+	<script src="js/common.js"></script>
+
+
+</body>
+</html>
